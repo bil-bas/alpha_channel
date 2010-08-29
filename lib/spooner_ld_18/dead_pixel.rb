99e83aeee4e92952f0cfc7398b014d38def186d8
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-08-29 14:49:03 +0100

    Since there are more dead pixels now, make them do less damage.

--- lib/spooner_ld_18/dead_pixel.rb
+++ lib/spooner_ld_18/dead_pixel.rb
@@ -4,7 +4,7 @@ class DeadPixel < Pixel
   def initialize(options = {})
     super({:color => Color.new(255, 0, 180, 0)}.merge! options )
     @original_health = @max_health = @health = 5000
-    @damage = 1
+    @damage = 0.5
   end
 
   def safe_distance
