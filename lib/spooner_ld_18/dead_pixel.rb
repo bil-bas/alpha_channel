a7cb72524a214c2f5b8b83d225a04ce96f86b63a
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-08-30 14:49:05 +0100

    Reduced movement speed of green, because they are supposed to be more static.

--- lib/spooner_ld_18/dead_pixel.rb
+++ lib/spooner_ld_18/dead_pixel.rb
@@ -3,7 +3,7 @@ require 'enemy'
 class DeadPixel < Enemy
   def max_health; 5000; end
   def damage; 0.5; end
-  def force; controlled? ? 50 : 0; end
+  def force; controlled? ? 30 : 0; end
   def num_kills; 0; end
   def initial_color; Color.new(255, 0, 255, 0); end
   def control_cost; 10; end
