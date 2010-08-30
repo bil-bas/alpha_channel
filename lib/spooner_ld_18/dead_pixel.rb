65366423635ca07e256d30525db7558a58524b38
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-08-30 13:05:22 +0100

    Stopped controlled green vs green from crashing. Increased cost for controlling a green and slowed its movement.

--- lib/spooner_ld_18/dead_pixel.rb
+++ lib/spooner_ld_18/dead_pixel.rb
@@ -3,17 +3,14 @@ require 'enemy'
 class DeadPixel < Enemy
   def max_health; 5000; end
   def damage; 0.5; end
-  def speed; controlled? ? 100 : 0; end
+  def speed; controlled? ? 50 : 0; end
   def num_kills; 0; end
   def initial_color; Color.new(255, 0, 255, 0); end
+  def control_cost; 10; end
   
   def initialize(space, options = {})
     super(space, options)
 
     shape.body.mass *= 100 # Dead pixels are a lot harder to push around.
-  end
-
-  def fight(other)
-    other.fight(self)
   end
 end
