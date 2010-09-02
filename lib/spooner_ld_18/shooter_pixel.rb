d9a96c1a02c49f92efab97f37c61a90b5f2bb497
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-09-02 17:42:16 +0100

    Changed the way that the shooter throws pixels at you.

--- lib/spooner_ld_18/shooter_pixel.rb
+++ lib/spooner_ld_18/shooter_pixel.rb
@@ -3,8 +3,9 @@ class ShooterPixel < Boss
   SHOOT_RANGE = 125
   PUSH_RANGE = 75
   INITIAL_COLOR = Color.new(255, 255, 0, 255)
-  SHOOT_FORCE = 10
+  SHOOT_FORCE = 100
   PUSH_FORCE = 1
+  MIN_SHOOT_DELAY = 1000
 
   def force; 1.2; end
   def damage; 5; end
@@ -14,23 +15,29 @@ class ShooterPixel < Boss
   def boss_update
     return unless player = Player.all.first
 
-    @shootee = nil
     nearest_distance = Float::INFINITY
     if player
-      # Find which one to shoot.
-      (Enemy.all - [self]).each do |pixel|
-        distance = distance_to(pixel)
-        if not pixel.controlled? and distance < SHOOT_RANGE and pixel.distance_to(player) < distance
-          if distance < nearest_distance
-            @shootee = pixel
-            nearest_distance = distance
+      @last_shot_at ||= $window.ms
+      if @shootee.nil? and (not controlled?) and ($window.ms - @last_shot_at > MIN_SHOOT_DELAY)
+        @shootee = nil
+        # Find which one to shoot.
+        (Enemy.all - [self]).each do |pixel|
+          distance = distance_to(pixel)
+          if not pixel.controlled? and distance < SHOOT_RANGE and pixel.distance_to(player) < distance
+            if distance < nearest_distance
+              @shootee = pixel
+              nearest_distance = distance
+            end
           end
         end
+
+        # Shoot the chosen idiot :)
+        if @shootee
+          @shootee.push(player.x, player.y, SHOOT_FORCE)
+          @last_shot_at = $window.ms
+        end
       end
-
-      # Shoot the chosen idiot :)
-      @shootee.push(player.x, player.y, SHOOT_FORCE) if @shootee
-
+      
       # Push those really close away.
       (Enemy.all - [self, @shootee]).each do |pixel|
         pixel.push(x, y, -PUSH_FORCE) if distance_to(pixel) < PUSH_RANGE and not pixel.controlled?
@@ -41,8 +48,9 @@ class ShooterPixel < Boss
   def draw
     super
 
-    if @shootee
+    if @shootee    
       @@glow.draw(@shootee.x - @@glow.width / 2, @shootee.y - @@glow.height / 2, zorder + 1, 1, 1, glow_color, :additive)
+      @shootee = nil if @shootee.shape.body.vel.x.abs < 200 and @shootee.shape.body.vel.y.abs < 200
     end
   end
 end
