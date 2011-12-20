030ca074ead14b72832c0cb0f30ecf32e3d03515
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-12-20 17:50:23 +0000

    Improved transition fades.

--- lib/alpha_channel/states/level_transition.rb
+++ lib/alpha_channel/states/level_transition.rb
@@ -1,7 +1,6 @@
 class LevelTransition < GameState
-  TRANSITION_DURATION = 3.0 # 3s to fade in/out.
-  FADE_OUT_DURATION = TRANSITION_DURATION / 2
-  FADE_IN_DURATION = TRANSITION_DURATION / 2
+  FADE_OUT_DURATION = 1.5
+  FADE_IN_DURATION = 1.5
 
   def initialize(level, options = {})
     super options
@@ -12,6 +11,7 @@ class LevelTransition < GameState
     
     @new_level = Level.new(level, options)
     @overlay_color = Color.rgba(0, 0, 0, 0)
+    @alpha = 0.0
     @fading = :out
 
     @sound = Sample["level.ogg"]
@@ -24,12 +24,14 @@ class LevelTransition < GameState
 
     if @fading == :in
       # Fade in.
-      @overlay_color.alpha -= ($window.frame_time * 255) / FADE_IN_DURATION
+      @alpha -= ($window.frame_time * 255) / FADE_IN_DURATION
+      @overlay_color.alpha = @alpha
 
       pop_game_state if @overlay_color.alpha <= 0
     else
       # Fade out.
-      @overlay_color.alpha += ($window.frame_time * 255) / FADE_OUT_DURATION
+      @alpha += ($window.frame_time * 255) / FADE_OUT_DURATION
+      @overlay_color.alpha = @alpha
 
       if previous_game_state.respond_to? :update_particles
         previous_game_state.update_particles
